e29c93d7e4699eeb646be7741f8f54d5e7ced68a
Rohith545355 <128197694+Rohith545355@users.noreply.github.com>
2023-04-14 22:39:49 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,12 @@
 <?php
 
-    $db_con = mysqli_connect("127.0.0.1","root","","isg");
+$db_host = "us-cdbr-east-06.cleardb.net";
+$db_user = "bf4b6baeca1403";
+$db_pass = "957c682e";
+$db_name = "heroku_e9f26e96f7ffdaa";
+    
+    #$db_con = mysqli_connect("127.0.0.1","root","","isg");
+    $db_con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
